714599bf646923a8d1d7bea331ad5715d63c527d
Kyle Kolomichuk <kylekolomichuk>
2021-05-25 10:22:47 -0700

    updating starting and ending indicies assignments for JSON parser (fix)

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -255,7 +255,7 @@ class WebServer {
             id = json.substring(startIDIndex, endIDIndex);
             json = json.substring(endIDIndex);
             builder.append(user + ", " + id + " -> " + name);
-          } while (json.indexOf("\"id\": ") != 0);
+          } while (json.indexOf("\"id\":") != 0);
           builder.append("HTTP/1.1 200 OK\n");
           builder.append("Content-Type: text/html; charset=utf-8\n");
           builder.append("\n");
